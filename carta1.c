c52bd271540397b936352371dd354f6d9abb4ab1
Oliver23170 <renatastos@hotmail.com>
2026-06-05 02:27:25 +0000

    ajuste carta1

---
+++ carta1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+
+    char Estado [20];
+    char cidade [23];
+    int populaçao =23;
+    float km2 =10.000;
+    int pontosturístico =40;
+
+    printf ("qual o estado: \n");
+    scanf ("%s", &Estado);
+
+    printf ("nome da cidade: \n");
+    scanf ("%d", &cidade);
+
+    printf ("numero de população: \n");
+    scanf ("%d", &populaçao);
+
+    printf ("area em km2: \n");
+    scanf ("%f", &km2);
+
+    printf ("numero de pontosturisticos: \n");
+    scanf ("%d", &pontosturístico);
+
+
+
+
+
+
+}
